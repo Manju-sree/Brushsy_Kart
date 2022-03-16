7e57a3c0d8b80d5a1b7b48ec6f72d4405ad24916
Manju-sree <manjushreek9@gmail.com>
2022-03-16 23:30:10 +0530

    added home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import logo from "./logo.png";
+import Home from "./pages/Home/HomePage";
+import{Routes, Route} from "react-router-dom";
 
 function App() {
-    return (< div className="App" >
-        Hello
-    </div>);
+  return (
+    <div className="App">
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
