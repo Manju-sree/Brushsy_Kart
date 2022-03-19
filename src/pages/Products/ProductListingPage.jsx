14d6bfc34cb5f193dd307daab06882368a1d74b6
Manju-sree <manjushreek9@gmail.com>
2022-03-19 12:56:32 +0530

    update : added new components to ProductList page

--- src/pages/Products/ProductListingPage.jsx
+++ src/pages/Products/ProductListingPage.jsx
@@ -1,8 +1,14 @@
+import { FilterBar } from "../../components";
 import { NavBarTopWithLogout } from "../../components/NavBarTopWithLogout/NavBarTopWithLogout";
+import { ProductCard } from "../../components/ProductCard/ProductCard";
 
 export const Products =()=>{
     return(<>
     {/* <!-- navigation bar --> */}
+    <div class="filter-container">
     <NavBarTopWithLogout/>
+    <FilterBar/>
+    <ProductCard/>
+    </div>
     </>);
 }
