a852adcd25d2b4579e282788aa465821fca138a2
Manju-sree <manjushreek9@gmail.com>
2022-03-19 12:55:23 +0530

    update : made changes in productcard component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,29 +1,49 @@
-import "../../styles/main.css";
-import React from 'react'
+import React from "react";
+import "../../styles/products.css";
+import { useEffect,useState } from "react";
+import axios from "axios";
 import { Link } from 'react-router-dom';
 import { FaStar, FaHeart,} from "react-icons/fa";
-export const ProductCard = () => {
-    <>
-        <div className="filter-grid-right">
-            {/* <!-- card with badge --> */}
-            <div className="card-vertical pos-rel card-container flex-items display-flex-column card-vertical-shadow">
+import {products} from "../../backend/db/products";
+
+
+export const ProductCard=()=>{
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        (async () => {
+            try {
+              const { data } = await axios.get("/api/products");
+              setData(data.products);
+             console.log(data);
+            } catch (error) {
+              console.log(error);
+            }
+          })();
+},[])
+    return(
+    <div className="filter-grid-right">
+        {data.map((item)=>{
+            return(<>
+        
+            
+             <div className="card-vertical pos-rel card-container flex-items display-flex-column card-vertical-shadow">
                 <div className="image-container badge-container pos-rel ">
-                    <img src={image} alt="product-image" className="img-responsive card-img"/>
+                    <img src={item.image} alt="product-image" className="img-responsive card-img"/>
                         
-                        {isOffer && (
+                        {item.isOffer && (
                     <span className="badge-on-card ">OFFER</span>
                   )}
-                      {isNew && (
+                     {item.isArrivedNew && (
                     <span className="badge-on-card ">NEW</span>
                   )}
                 </div>
-                {!inStock &&( <div className="text-overlay">
-                    <div className="text-overlay-size dark-primary-color">Out of stock</div>
-                </div>)}
+                <div className={`${item.inStock?"":"text-overlay"}`}>
+                    <div className={`${item.inStock?" ":"text-overlay-size dark-primary-color"}`}>{`${item.inStock?" ":"Out of stock"}`}</div>
+                </div>
                
                 <div className="card-text-container pos-rel flex-grow display-flex-column">
                     <div className="text-container-title">
-                        <h6 className="product-title pos-rel">{productName}
+                        <h6 className={`${item.inStock?"product-title pos-rel":"product-title disable-font pos-rel"}`}>{item.productName}
                             <button className="like-btn link-no-style">
                                 <span><FaHeart className="wish-icon"/></span>
                             </button>
@@ -31,18 +51,18 @@ export const ProductCard = () => {
 
                     </div>
                     <div className="text-container-desc  flex-grow display-flex-column">
-                        <span className=" text-price-lighter"> Rs.{price}
-                            <small className="discount-txt">({offer})</small>
+                        <span className=" text-price-lighter"> Rs.{item.price}
+                            <small className="discount-txt">  {item.offer}</small>
                         </span>
-                        <pre className="rating-text">{rating}<FaStar className="rating"/> |{ratingbymembers}
+                        <pre className="rating-text">{item.rating}<FaStar className="rating"/> |{item.ratingbymembers}
                         </pre>
                     </div>
                     <div className="CTA-container">
-                        <button className="btn-outline btn-on-hover wd-100-pc mr-bt ">
-                            <Link to="/Cart" className="link-no-style">Add to Cart</Link>
-                        </button>
-                        {!inStock && (<button className="btn-outline btn-disabled  wd-100-pc mr-bt ">
+                        
+                        {!item.inStock? (<button className="btn-outline btn-disabled  wd-100-pc mr-bt ">
                             <Link to="#" className="link-no-style">Out of Stock</Link>
+                        </button>):(<button className="btn-outline btn-on-hover wd-100-pc mr-bt ">
+                            <Link to="/Cart" className="link-no-style">Add to Cart</Link>
                         </button>)}
                         
                         <button className="btn-outline btn-on-hover wd-100-pc mr-bt  ">
@@ -50,7 +70,8 @@ export const ProductCard = () => {
                         </button>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
+            </div> 
+            </>);
+        })}
+        </div>);
 }
