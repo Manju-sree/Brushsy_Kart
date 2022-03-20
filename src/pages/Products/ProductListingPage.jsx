4c80c268aaab04d9280a3fbac5ac6f4340760141
Manju-sree <manjushreek9@gmail.com>
2022-03-20 18:36:03 +0530

    feat : added productlist to products page from useFilter context

--- src/pages/Products/ProductListingPage.jsx
+++ src/pages/Products/ProductListingPage.jsx
@@ -1,14 +1,17 @@
-import { FilterBar } from "../../components";
-import { NavBarTopWithLogout } from "../../components/NavBarTopWithLogout/NavBarTopWithLogout";
-import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { FilterSideBar, ProductCard} from "../../components/index";
+import { useFilter } from "../../context/FilterContext/FilterProvider";
 
-export const Products =()=>{
-    return(<>
-    {/* <!-- navigation bar --> */}
-    <div class="filter-container">
-    <NavBarTopWithLogout/>
-    <FilterBar/>
-    <ProductCard/>
-    </div>
-    </>);
-}
+export const Products = () => {
+
+  const {productList} = useFilter();
+  return (
+    <>
+      <div class="filter-container">
+        <FilterSideBar />
+        <div className="filter-grid-right">
+        {productList.map((prod)=><ProductCard key={prod._id} product={prod}/>)}
+        </div>
+      </div>
+    </>
+  );
+};
