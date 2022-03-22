4d77b2088aba4dbae21d98b487087209bbfbd7d2
Manju-sree <manjushreek9@gmail.com>
2022-03-22 18:10:25 +0530

    Feat : added wishListcard

--- src/components/WishListCard/WishListCard.jsx
+++ src/components/WishListCard/WishListCard.jsx
@@ -2,8 +2,12 @@ import '../../styles/main.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaStar, FaHeart } from 'react-icons/fa';
+import { useCartAndWishList } from "../../context/index";
+import "../ProductCard/ProductCard";
 
-export const WishListCard = ({product}) => {
+export const WishListCard = ({ product }) => {
+    const { wishListState, wishListDispatch, cartState, cartDispatch } = useCartAndWishList();
+    const { wishListItem } = wishListState;
     const {
         id,
         productName,
@@ -13,13 +17,16 @@ export const WishListCard = ({product}) => {
         ratingbymembers,
         image,
         categoryName,
-    }= product;
+    } = product;
     return (
-        <>
-        <h1 className="wishlist-title">My Wishlist <span className="wishlist-subtitle">(6 items)</span></h1>
-    <div className="wishlist-card-container">
         <div className="card-vertical pos-rel card-container flex-items display-flex-column card-vertical-shadow">
-            <button type="button" className="btn-close"></button>
+            <button type="button" className="btn-close"
+            onClick={() =>
+                wishListDispatch({
+                    type: "REMOVE_ITEM_FROM_WISHLIST",
+                    payload: product
+                })
+            } ></button>
             <div className="image-container  pos-rel ">
                 <img src={image} alt={categoryName} className="img-responsive card-img" />
             </div>
@@ -30,7 +37,6 @@ export const WishListCard = ({product}) => {
                             <span><FaHeart className="wished-icon" /></span>
                         </button>
                     </h6>
-
                 </div>
                 <div className="text-container-desc  flex-grow display-flex-column">
                     <span className=" text-price-lighter"> Rs.{price}
@@ -41,12 +47,14 @@ export const WishListCard = ({product}) => {
                 </div>
                 <div className="CTA-container">
                     <button className="btn-outline btn-on-hover wd-100-pc">
-                        <Link href="/pages/cart.html" classNameName="link-no-style">Add to Cart</Link>
+                        <Link to="/Cart" className="link-no-style"
+                            onClick={() =>
+                                cartDispatch({ type: "ADD_ITEM_TO_CART", payload: product })
+                            }>Move to Cart</Link>
                     </button>
                 </div>
             </div>
         </div>
-    </div>
-    </>
-        );
+
+    );
 }
