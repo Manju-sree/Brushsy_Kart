20cb04ee39ea62dcddf0a583c560ca766ee3e191
Manju-sree <manjushreek9@gmail.com>
2022-03-22 18:13:28 +0530

    update : done changes in wishlistpage and added its context to index.js

--- src/pages/WishList/WishListPage.jsx
+++ src/pages/WishList/WishListPage.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ErrorMessage, WishListCard } from "../../components/index";
+import { useCartAndWishList } from "../../context/index";
 
-
-export const WishList=()=>{
-    return(<>
-  
-    </>);
-}
+export const WishList = () => {
+  const { wishListState, wishListDispatch } = useCartAndWishList();
+  const { wishListItem, wishListCount } = wishListState;
+  return (
+    <>
+      <h1 className="wishlist-title">
+        My Wishlist{" "}
+        <span className="wishlist-subtitle">({wishListCount}-items)</span>
+      </h1>
+      <div className="wishlist-card-container">
+        {wishListItem.length === 0 ? (
+          <Link to="/WishList">
+            <ErrorMessage message={"No Products ☹ in WishList"} />
+          </Link>
+        ) : (
+          wishListItem.map((product) => {
+            return <WishListCard product={product} key={product._id} />;
+          })
+        )}
+      </div>
+    </>
+  );
+};
