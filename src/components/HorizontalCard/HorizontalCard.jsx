6d47f7c503e2d2b606c7cc21c57bf4a04fb84b16
Manju-sree <manjushreek9@gmail.com>
2022-04-03 19:05:17 +0530

    update: remove extra links in horizontalcard of cart page

--- src/components/HorizontalCard/HorizontalCard.jsx
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -22,7 +22,7 @@ export const HorizontalCard = ({ item }) => {
     //Add item to wishlist from cart and removing that item from cart.
 
     const addToWishListRemoveFromCart = (product) => {
-        const newWishlistItem = wishListItem.some(
+        const newWishlistItem = wishListItem.find(
             (item) => item._id === product._id
         );
 
@@ -52,41 +52,38 @@ export const HorizontalCard = ({ item }) => {
                 <div className="CTA-container">
 
                     {item.qty === 1 ? (
+                        <button className="card-btn qty-btn-color border-sqr">
                         <FaTrashAlt
                             onClick={() =>
                                 cartDispatch({ type: "REMOVE_ITEM_FROM_CART", payload: item })
                             }
                         />
+                        </button>
                     ) : (
                         <button className="card-btn qty-btn-color border-sqr">
-                            <span className="qty-btn-icon">
                                 <BiMinus
                                     onClick={() =>
                                         cartDispatch({ type: "DECREMENT_QUANTITY", payload: item })
                                     }
                                 />
-                            </span>
                         </button>
                     )}
                     <span className="cart-qty-style">{qty}</span>
                     <button className="card-btn qty-btn-color border-sqr">
-                        <span className="qty-btn-icon">
                             <BiPlus
                                 onClick={() =>
                                     cartDispatch({ type: "INCREMENT_QUANTITY", payload: item })
                                 }
                             />
-                        </span>
                     </button>
                     <div className="align-inline">
-                        <button className="cart-btn-outline btn-on-hover margin-btn">
-                            <Link to="/WishList" className="link-no-style" onClick={() => addToWishListRemoveFromCart(item)}>Move to Wishlist</Link>
+                        <button className="cart-btn-outline btn-on-hover margin-btn"
+                            onClick={() => addToWishListRemoveFromCart(item)}>Move to Wishlist
                         </button>
-                        <button className="cart-btn-outline btn-on-hover margin-btn">
-                            <Link to="#" className="link-no-style"
+                        <button className="cart-btn-outline btn-on-hover margin-btn"
                                 onClick={() =>
                                     cartDispatch({ type: "REMOVE_ITEM_FROM_CART", payload: item })
-                                }>Remove from cart</Link>
+                                }>Remove from cart
                         </button>
                     </div>
                 </div>
