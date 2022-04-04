d1b067fddfcbae1f57982ffa7e20bc21a3fc2aec
Manju-sree <manjushreek9@gmail.com>
2022-04-04 15:51:27 +0530

    updated home page with Brands component

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,6 +1,6 @@
 import "../../styles/main.css";
 import { Link } from "react-router-dom";
-import { FeaturedCard ,OfferCards} from "../../components/index";
+import { FeaturedCard ,OfferCards,Brands} from "../../components/index";
 
 export const Home = () => {
     return (<>
@@ -21,29 +21,6 @@ export const Home = () => {
         </div>
         <OfferCards/>
         <FeaturedCard/>
-        {/* <!-- brands --> */}
-        <div className="brands">
-            <div className="small-container">
-                <h2 className="feat-title">Brands</h2>
-                <div className="row-container">
-                    <div className="container-col-5">
-                        <img src="/assets/images/brand_logos/colorbar.png" alt="colorbar" />
-                    </div>
-                    <div className="container-col-5">
-                        <img src="/assets/images/brand_logos/miniso_brand.png" alt="miniso" />
-                    </div>
-                    <div className="container-col-5">
-                        <img src="/assets/images/brand_logos/sugar_brand.png" alt="sugar" />
-                    </div>
-                    <div className="container-col-5">
-                        <img src="/assets/images/brand_logos/pac_brand.png" alt="pac" />
-                    </div>
-                    <div className="container-col-5">
-                        <img src="/assets/images/brand_logos/mac_brand.png" alt="mac" />
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <Brands/>  
     </>);
 }
