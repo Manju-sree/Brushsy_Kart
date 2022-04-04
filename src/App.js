26f0a0214fa0a772a8202df25387d4b887602875
Manju-sree <manjushreek9@gmail.com>
2022-04-05 00:48:26 +0530

    created Protected routes for cart and wishlist page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Home,Products,Cart,WishList,SignUp,Login,Logout} from "./pages/index";
 import { NavBarTop,Error404 } from "./components";
 import MockMan from "mockman-js";
 import{Routes, Route} from "react-router-dom";
+import { ProtectedRoutes } from "./routes/ProtectedRoutes";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Products" element={<Products />} />
-        <Route path="/WishList" element={<WishList />} />
-        <Route path="/Cart" element={<Cart />} />
+        <Route path="/WishList" element={<ProtectedRoutes authToPages={<WishList />}></ProtectedRoutes>}></Route>
+        <Route path="/Cart" element={<ProtectedRoutes authToPages={<Cart />}></ProtectedRoutes>}></Route>
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Login" element={<Login/>} />
         <Route path="/Logout" element={<Logout/>} />
