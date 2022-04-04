58fc6bec9a3c54a821acd276499d8c5c1c2e09ec
Manju-sree <manjushreek9@gmail.com>
2022-04-05 00:46:47 +0530

    created AuthConstants for storing initial states

---
+++ src/constants/Authentication/AuthConstants.js
@@ -0,0 +1,14 @@
+export const initialLogInData = {
+    email: "",
+    password: ""
+}
+export const initialSignUpData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+}
+export const loginAsGuestCredentials = {
+    email: "johndoe@gmail.com",
+    password: "johnDoe123"
+}
